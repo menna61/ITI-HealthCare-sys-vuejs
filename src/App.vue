c9ffb7361a064e35485cd9d7c0057f36bdc158b6
SalemaKhlaed <salmakhaled1999iti@gmail.com>
2025-10-16 17:53:04 +0300

    Replaced old project with full updated files

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+<template>
+  <div id="app">
+ <div   class="header flex w-[100%] pl-[80px] pr-[80px] pt-[15px] pb-[15px]">
+  <img src="/assets/logo - colors.svg">
+
+  <nav>
+    <a href="#">home</a>
+    <a href="#">About Us</a>
+    <a href="#">Services</a>
+    <a href="#">Contact Us</a>
+  </nav>
+<div class="btns">
+  <button class="btn-lo">Log in</button>
+  <button class="btn-si">Sign up</button>
+</div>
+ </div>
+ <!--     gh  -->
+<div class="home flex justify-evenly">
+  <div class="text">
+    <p>Your Compelete</p>
+<p>Digital <span class="one">healthcare</span> </p>
+<p>Experience</p>
+
+<span class="two">
+  <p>Your all-in-one plattform fpr appointments and online consultation, stay </p>
+  <p>connected with your doctor and manage your health</p></span>
+<button>get started</button>
+  </div>
+
+  <div>  <img src="/assets/doctors-hospital.png"></div>
+</div>
+
+</div>
+
+</template>
+
+<script>
+// export default{
+//   name:"app",
+// data(){
+
+// }
+// }
+
+</script>
+
+<style scoped>
+
+</style>
