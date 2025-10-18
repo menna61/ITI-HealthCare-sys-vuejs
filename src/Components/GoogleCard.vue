91f7316fd3b4872c92a32ab3e461b7b3465b140e
menna sayed <mennasayed750@gmail.com>
2025-10-18 10:08:43 +0300

    changed some texts

--- src/Components/GoogleCard.vue
+++ src/Components/GoogleCard.vue
@@ -1,13 +1,19 @@
 <template>
-    <div class="google flex w-full border border-gray-200 py-4 items-center justify-center rounded-lg gap-6">
+    <div class="google flex w-full border border-gray-200 py-4 items-center justify-center rounded-lg gap-6 cursor-pointer">
         <img src="../assets/google.svg" alt="">
-        <p class="text-gray-500">sign up with google</p>
+        <p v-if="isSignup" class="text-gray-500">Sign up with google</p>
+        <p v-else class="text-gray-500">Continue with google</p>
     </div>
 </template>
 
 <script>
     export default {
-        name:"GoogleCard"
+        name:"GoogleCard",
+        computed:{
+            isSignup(){
+                return this.$route.path === '/doctorSignup' || this.$route.path === '/patientSignup'
+            }
+        }
     }
 </script>
 
